69eee2d892c954d2491675370c11370fc0166f41
Alex Moldovan <alex.n.moldovan@gmail.com>
2022-10-05 19:07:03 +0000

    agenda animation

--- src/data/agenda.ts
+++ src/data/agenda.ts
@@ -9,4 +9,8 @@ export const agenda: AgendaItem[] = [
   { type: "talk", speakers: ["carolyn"], timeSlot: "09:30 - 10:00" },
   { type: "talk", speakers: ["anjana"], timeSlot: "10:00 - 10:30" },
   { type: "placeholder", content: "Coffee Break", timeSlot: "10:30 - 11:00" },
+  { type: "talk", speakers: ["stefan"], timeSlot: "11:00 - 11:30" },
+  { type: "talk", speakers: ["sara"], timeSlot: "11:30 - 12:00" },
+  { type: "talk", speakers: ["carolyn"], timeSlot: "12:00 - 12:30" },
+  { type: "placeholder", content: "Lunch Break", timeSlot: "12:30 - 14:00" },
 ];
