568c4d172ba13ba0c5b2ffd55d14e856adff5941
Alexandru Lupu <alexandrulupu89@gmail.com>
2023-02-22 14:37:36 +0200

    chore: add google analytics tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [partytown()],
+});
