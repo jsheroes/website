c8eae0d7180c7ac5821cc37821c80adfb1100722
Alex Moldovan <alex.n.moldovan@gmail.com>
2023-03-18 20:09:40 +0000

    content: speaker talks

---
+++ src/data/partners.ts
@@ -0,0 +1,9 @@
+import type { Organization } from "./types";
+
+export const partners: Organization[] = [
+  {
+    name: "Snyk",
+    url: "https://snyk.io/",
+    imgName: "partners/logo_snyk.png",
+  },
+];
