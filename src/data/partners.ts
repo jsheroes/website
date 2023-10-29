0a08455bf438436bca2903127472afa3d90a863e
Alexandru Lupu <alexandrulupu89@gmail.com>
2023-10-29 20:24:27 +0200

    Removed sponsors and partners. Moved Accessibility, Eco-Friendly and Handbook pages

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -1,14 +1,3 @@
 import type { Organization } from "./types";
 
-export const partners: Organization[] = [
-  {
-    name: "Snyk",
-    url: "https://snyk.io/",
-    imgName: "partners/logo_snyk.png",
-  },
-  {
-    name: "Twilio",
-    url: "https://www.twilio.com/",
-    imgName: "partners/logo_twilio.png",
-  },
-];
+export const partners: Organization[] = [];
