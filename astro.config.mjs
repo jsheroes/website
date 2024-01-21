3888449ce23c8eb636bab264a455e18409b4c103
AlexandruLupu <alexandrulupu89@gmail.com>
2024-01-21 12:05:14 +0200

    Removed compress integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,9 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
-import compress from "astro-compress";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    mdx(),
-    compress({
-      CSS: false,
-      HTML: false,
-      JavaScript: true,
-      SVG: true,
-      Image: false,
-    }),
-  ],
+  integrations: [mdx()],
   redirects: {
     "/tags": "/blog",
   },
