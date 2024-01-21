2cd828f807a06ddc594d76213b681f886e012f28
Alex Moldovan <alex.n.moldovan@gmail.com>
2024-01-21 11:23:53 +0000

    feat: setup tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), tailwind()],
   redirects: {
-    "/tags": "/blog",
-  },
+    "/tags": "/blog"
+  }
 });
