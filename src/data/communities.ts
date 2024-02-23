ec071b4cd8a34fa9c34b105fde119526ab910a93
Alex Moldovan <alex.n.moldovan@gmail.com>
2024-02-23 16:12:34 +0000

    chore: cleanup partners

--- src/data/communities.ts
+++ src/data/communities.ts
@@ -17,31 +17,6 @@ export const communities: Organization[] = [
     imgName: "communities/logo_ngbucharest.png",
   },
   {
-    name: "Frontend Hub Iasi",
-    url: "https://www.facebook.com/frontendhub/",
-    imgName: "communities/logo_frontend_hub.png",
-  },
-  {
-    name: "Voxxed Days Cluj",
-    url: "https://romania.voxxeddays.com/cluj-napoca/",
-    imgName: "communities/logo_voxxedday.png",
-  },
-  {
-    name: "dotnetdays",
-    url: "https://dotnetdays.ro/",
-    imgName: "communities/logo_dotnetdays.png",
-  },
-  {
-    name: "GoConf",
-    url: "https://goconf.io/",
-    imgName: "communities/logo_goconf.png",
-  },
-  {
-    name: "AgentConf",
-    url: "https://www.alpine-conferences.com/agent-conf-2023/",
-    imgName: "communities/logo_agentconf.png",
-  },
-  {
     name: "JSNation",
     url: "https://jsnation.com/",
     imgName: "communities/logo_jsnation.png",
@@ -60,11 +35,6 @@ export const communities: Organization[] = [
     name: "FrontEnd.ro",
     url: "https://frontend.ro/",
     imgName: "communities/logo_frontendro.png",
-  },
-  {
-    name: "ReactNative EU",
-    url: "https://www.react-native.eu/?utm_campaign=React_Native_EU&utm_source=website&utm_content=jsheroes_io",
-    imgName: "communities/logo_reactnativeeu.png",
   },
   {
     name: "QueerJS",
