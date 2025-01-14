603317ab04adc72a42bac4cc889a2fec95866da9
Alex Moldovan <alex.n.moldovan@gmail.com>
2025-01-14 11:28:03 +0200

    add sponsor

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -1,49 +1,13 @@
 import type { Organization } from "./types";
 
-export const sponsors: Organization[] = [
-  // {
-  //   name: "Accesa",
-  //   url: "https://careers.accesa.eu/",
-  //   imgName: "sponsors/logo_accesa.png",
-  // },
-  // {
-  //   name: "Cloudflight",
-  //   url: "http://www.cloudflight.io/",
-  //   imgName: "sponsors/logo_cloudflight.jpg",
-  // },
-  // {
-  //   name: "flowx.ai",
-  //   url: "https://www.flowx.ai/",
-  //   imgName: "sponsors/logo_flowx.ai.png",
-  // },
-  // {
-  //   name: "GlobalLogic",
-  //   url: "https://www.globallogic.com/ro/",
-  //   imgName: "sponsors/logo_globalLogic.png",
-  // },
-  // {
-  //   name: "Evozon",
-  //   url: "https://www.evozon.com/en/",
-  //   imgName: "sponsors/logo_evozon.png",
-  // },
-  // {
-  //   name: "QIAGEN Digital Insights",
-  //   url: "https://digitalinsights.qiagen.com/",
-  //   imgName: "sponsors/logo_qiagen.svg",
-  // },
-  // {
-  //   name: "Betfair",
-  //   url: "https://www.betfairromania.ro/",
-  //   imgName: "sponsors/logo_betfair.svg",
-  // },
-  // {
-  //   name: "Cloud Wizard",
-  //   url: "https://cloudwizard.tech/",
-  //   imgName: "sponsors/logo_cloud-wizard.svg",
-  // },
-  // {
-  //   name: "Siemens",
-  //   url: "https://www.siemens.com/ro/ro/companie/siemens-romania-rd.html",
-  //   imgName: "sponsors/logo_siemens.png",
-  // },
+export const goldSponsors: Organization[] = [
+  {
+    name: "GlobalLogic",
+    url: "https://www.globallogic.com/ro/",
+    imgName: "sponsors/logo_globalLogic.png",
+  },
 ];
+
+export const silverSponsors: Organization[] = [];
+
+export const bronzeSponsors: Organization[] = [];
