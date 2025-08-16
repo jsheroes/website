4f34269d50458930f9876b382bf27b5e0ea322d5
Alex Moldovan <alex.n.moldovan@gmail.com>
2025-08-16 09:15:29 +0300

    add squiggleconf as partner

--- src/data/communities.ts
+++ src/data/communities.ts
@@ -22,6 +22,11 @@ export const communities: Organization[] = [
     imgName: "communities/logo_jsnation.png",
   },
   {
+    name: "SquiggleConf",
+    url: "https://squiggleconf.com/",
+    imgName: "communities/logo_squiggleconf.svg",
+  },
+  {
     name: "CityJS",
     url: "https://cityjsconf.org/",
     imgName: "communities/logo_cityjs.png",
@@ -31,11 +36,6 @@ export const communities: Organization[] = [
     url: "https://code4.ro/",
     imgName: "communities/logo_code4ro.png",
   },
-  // {
-  //   name: "FrontEnd.ro",
-  //   url: "https://frontend.ro/",
-  //   imgName: "communities/logo_frontendro.png",
-  // },
   {
     name: "QueerJS",
     url: "https://queerjs.com/",
@@ -46,21 +46,6 @@ export const communities: Organization[] = [
     url: "https://www.grusp.org/en/",
     imgName: "communities/logo_grusp.png",
   },
-  // {
-  //   name: "Codemotion",
-  //   url: "https://www.codemotion.com/",
-  //   imgName: "communities/logo_codemotion.png",
-  // },
-  // {
-  //   name: "React Vienna",
-  //   url: "https://www.meetup.com/reactvienna/",
-  //   imgName: "communities/logo_reactvienna.png",
-  // },
-  // {
-  //   name: "React India",
-  //   url: "https://www.reactindia.io/",
-  //   imgName: "communities/logo_reactindia.png",
-  // },
   {
     name: "React Paris",
     url: "https://react.paris/",
