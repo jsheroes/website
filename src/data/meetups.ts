56d30b1f8c1c6d4680957ece9ab3c8f76b53e353
Alex Moldovan <alex.n.moldovan@gmail.com>
2026-02-07 17:36:50 +0200

    meetups page

---
+++ src/data/meetups.ts
@@ -0,0 +1,27 @@
+export type Meetup = {
+  title: string;
+  date: string;
+  location: string;
+  link: string;
+};
+
+export const meetups: Meetup[] = [
+  {
+    title: "Stripe Essentials: Powering Modern Payment Solutions",
+    date: "2025-10-21",
+    location: "GlobalLogic",
+    link: "https://www.meetup.com/jsheroes/events/311509249/",
+  },
+  {
+    title: "Workshop Day @ RebelDot",
+    date: "2025-11-15",
+    location: "RebelDot",
+    link: "https://www.meetup.com/jsheroes/events/311632124/",
+  },
+  {
+    title: "Colocation is King",
+    date: "2026-01-29",
+    location: "Wolfpack Digital",
+    link: "https://www.meetup.com/jsheroes/events/312933420/",
+  },
+];
